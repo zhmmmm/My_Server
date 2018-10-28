2f7c57ac76dc318ccfa078512610aaa4a04ba87b
15982683979 <15982683979@163.com>
2018-10-28 17:54:57 +0800

    m

--- Server/Main.cpp
+++ Server/Main.cpp
@@ -1,7 +1,9 @@
 // Server.cpp : Defines the entry point for the console application.
+
 #include "winsock2.h"
 #pragma comment(lib, "ws2_32.lib")
 #include <iostream>
+#include <Windows.h>
 using namespace std;
 
 int InitSocket()
@@ -37,7 +39,7 @@ int BindingSocket(int &RetValue, SOCKET &sServer, SOCKADDR_IN &addrServ)
 	}
 	return 2;
 }
-int StartMonitor(int &retVal,SOCKET &sServer)
+int StartMonitor(int &retVal, SOCKET &sServer)
 {
 	retVal = listen(sServer, 1);
 	if (SOCKET_ERROR == retVal)
@@ -49,7 +51,7 @@ int StartMonitor(int &retVal,SOCKET &sServer)
 	}
 	return 2;
 }
-int ReceiveClientRequest(sockaddr_in &addrClient,SOCKET &sServer,SOCKET &sClient)
+int ReceiveClientRequest(sockaddr_in &addrClient, SOCKET &sServer, SOCKET &sClient)
 {
 	int addrClientlen = sizeof(addrClient);
 	sClient = accept(sServer, (sockaddr FAR*)&addrClient, &addrClientlen);
@@ -65,62 +67,147 @@ int ReceiveClientRequest(sockaddr_in &addrClient,SOCKET &sServer,SOCKET &sClient
 
 int main()
 {
-	const int BUF_SIZE = 64;
-	WSADATA            wsd;            //WSADATA变量
-	SOCKET            sServer;        //服务器套接字
-	SOCKET            sClient;        //客户端套接字
-	SOCKADDR_IN        addrServ;        //服务器地址
-	char            buf[BUF_SIZE];    //接收数据缓冲区
-	char            sendBuf[BUF_SIZE];//返回给客户端得数据
-	int                retVal;            //返回值
-	//初始化套结字动态库
-	InitSocket();
+	//const int BUF_SIZE = 64;
+	//WSADATA            wsd;            //WSADATA变量
+	//SOCKET            sServer;        //服务器套接字
+	//SOCKET            sClient;        //客户端套接字
+	//SOCKADDR_IN        addrServ;        //服务器地址
+	//char            buf[BUF_SIZE];    //接收数据缓冲区
+	//char            sendBuf[BUF_SIZE];//返回给客户端得数据
+	//int                retVal;            //返回值
+	////初始化套结字动态库
+	//InitSocket();
+	////创建套接字
+	//CreateSocket(sServer);
+	////服务器套接字地址 
+	//addrServ.sin_family = AF_INET;
+	//addrServ.sin_port = htons(4999);
+	//addrServ.sin_addr.s_addr = INADDR_ANY;
+	////绑定套接字
+	//BindingSocket(retVal,sServer,addrServ);
+	////开始监听 
+	//StartMonitor(retVal,sServer);
+	////接受客户端请求
+	//sockaddr_in addrClient;
+	////int addrClientlen = sizeof(addrClient);
+	//ReceiveClientRequest(addrClient,sServer, sClient);
+	//while (true)
+	//{
+	//	//接收客户端数据
+	//	ZeroMemory(buf, BUF_SIZE);
+	//	retVal = recv(sClient, buf, BUF_SIZE, 0);
+	//	if (SOCKET_ERROR == retVal)
+	//	{
+	//		cout << "recv failed!" << endl;
+	//		closesocket(sServer);    //关闭套接字
+	//		closesocket(sClient);    //关闭套接字        
+	//		WSACleanup();            //释放套接字资源;
+	//		return -1;
+	//	}
+	//	if (buf[0] == '0'){ break; }
+	//	cout << "客户端发送的数据: " << buf << endl;
+	//	cout << "向客户端发送数据: ";
+	//	cin >> sendBuf;
+	//	send(sClient, sendBuf, strlen(sendBuf), 0);
+	//}
+	////退出
+	//closesocket(sServer);    //关闭套接字
+	//closesocket(sClient);    //关闭套接字
+	//WSACleanup();            //释放套接字资源;
 
-	//创建套接字
-	CreateSocket(sServer);
-
-	//服务器套接字地址 
-	addrServ.sin_family = AF_INET;
-	addrServ.sin_port = htons(4999);
-	addrServ.sin_addr.s_addr = INADDR_ANY;
-	//绑定套接字
-	BindingSocket(retVal,sServer,addrServ);
-
-	//开始监听 
-	StartMonitor(retVal,sServer);
-
-	//接受客户端请求
-	sockaddr_in addrClient;
-	//int addrClientlen = sizeof(addrClient);
-	ReceiveClientRequest(addrClient,sServer, sClient);
-
-	while (true)
+	int num1 = 30;
+	int num2 = 40;
+	HMODULE hDll = LoadLibraryA("..\\Debug\\ServerDll.dll");
+	typedef int(*Function)(int&, int&);
+	typedef int(*__InitSocket)();
+	typedef int(*__CreateSocket)(SOCKET&);
+	typedef int(*__BindingSocket)(int&, SOCKET&,u_short);
+	typedef int(*__StartMonitor)(int&, SOCKET&);
+	typedef int(*__ReceiveClientRequest)(SOCKET&,SOCKET&);
+	typedef int(*__ReceiveClientData)(SOCKET&,SOCKET&,char*,int,int&);
+	typedef int(*__ServerLogic)(char *Data);
+	typedef int(*__SendClientData)(SOCKET&, char*);
+	typedef int(*__ExitServer)(SOCKET&, SOCKET&);
+	if (hDll)
 	{
-		//接收客户端数据
-		ZeroMemory(buf, BUF_SIZE);
-		retVal = recv(sClient, buf, BUF_SIZE, 0);
-		if (SOCKET_ERROR == retVal)
+		Function function = (Function)GetProcAddress(hDll, "Function");
+		int num = function(num1,num2);
+		std::cout << "Ok " << num << std::endl;
+		std::cout << "Num1:" << num1 << std::endl;
+		std::cout << "Num2:" << num2 << std::endl;
+
+		SOCKET sServer;
+		SOCKET sClient;
+		int RetValue;
+		char Buf[64] = { 0 };
+		u_short Port = 4999;
+
+		__InitSocket InitSocket = (__InitSocket)GetProcAddress(hDll,"InitSocket");
+		int InitSocketReturn = InitSocket();
+		if (InitSocketReturn == 2)
 		{
-			cout << "recv failed!" << endl;
-			closesocket(sServer);    //关闭套接字
-			closesocket(sClient);    //关闭套接字        
-			WSACleanup();            //释放套接字资源;
-			return -1;
+			std::cout << "初始化成功！" << std::endl;
 		}
-		if (buf[0] == '0'){ break; }
+		__CreateSocket CreateSocket = (__CreateSocket)GetProcAddress(hDll, "CreateSocket");
+		int CreateSocketReturn = CreateSocket(sServer);
+		if (CreateSocketReturn == 2)
+		{
+			std::cout << "创建套字节成功！" << std::endl;
+		}
+		__BindingSocket BindingSocket = (__BindingSocket)GetProcAddress(hDll, "BindingSocket");
+		int BindingSocketReturn = BindingSocket(RetValue,sServer, Port);
+		if (BindingSocketReturn == 2)
+		{
+			std::cout << "绑定套字节成功！" << std::endl;
+		}
+		__StartMonitor StartMonitor = (__StartMonitor)GetProcAddress(hDll, "StartMonitor");
+		int StartMonitorReturn = StartMonitor(RetValue,sServer);
+		if (StartMonitorReturn == 2)
+		{
+			std::cout << "开始监听成功！" << std::endl;
+		}
+		__ReceiveClientRequest ReceiveClientRequest = (__ReceiveClientRequest)GetProcAddress(hDll, "ReceiveClientRequest");
+		int ReceiveClientRequestReturn = ReceiveClientRequest(sServer,sClient);
+		if (ReceiveClientRequestReturn == 2)
+		{
+			std::cout << "接受客户端请求成功！" << std::endl;
+		}
+		while (true)
+		{
+			__ReceiveClientData ReceiveClientData = (__ReceiveClientData)GetProcAddress(hDll, "ReceiveClientData");
+			int ReceiveClientDataReturn = ReceiveClientData(sServer,sClient,Buf,64,RetValue);
+			if (ReceiveClientDataReturn == 1)
+			{
+				std::cout << "服务器接受成功！" << std::endl;
+			}
+			std::cout << "服务器收到：" << Buf << std::endl;
 
-		cout << "客户端发送的数据: " << buf << endl;
+			__SendClientData SendClientData = (__SendClientData)GetProcAddress(hDll, "SendClientData");
+			char SendData[64] = { 0 };
+			std::cout << "输入向客户端的消息：";
+			std::cin >> SendData;
+			int SendClientDataReturn = SendClientData(sClient, SendData);
+			if (SendClientDataReturn == 1)
+			{
+				std::cout << "发送成功！" << std::endl;
+			}
+		}
 
-		cout << "向客户端发送数据: ";
-		cin >> sendBuf;
 
-		send(sClient, sendBuf, strlen(sendBuf), 0);
+		__ExitServer ExitServer = (__ExitServer)GetProcAddress(hDll, "ExitServer");
+		int ExitServerReturn = ExitServer(sServer,sClient);
+		if (ExitServerReturn == 0)
+		{
+			std::cout << "退出成功！" << std::endl;
+		}
 	}
+	else
+	{
+		return 0;
+	}
+	FreeLibrary(hDll);
+	hDll = NULL;
 
-	//退出
-	closesocket(sServer);    //关闭套接字
-	closesocket(sClient);    //关闭套接字
-	WSACleanup();            //释放套接字资源;
-
+	system("pause");
 	return 0;
 }
